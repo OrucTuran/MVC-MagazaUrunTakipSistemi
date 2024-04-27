2badf7ed60ad15ba4334748697be932a4e0ff01b
oructuran0 <orctrn193@gmail.com>
2024-04-28 01:35:54 +0300

    Musteri Listeleme Islemi

---
+++ MagazaUrunTakip/MagazaUrunTakip/Controllers/MusteriController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MagazaUrunTakip.Models.Entitiy;
+
+namespace MagazaUrunTakip.Controllers
+{
+    public class MusteriController : Controller
+    {
+        // GET: Musteri
+        DbMvcStokEntities db = new DbMvcStokEntities();
+        public ActionResult Index()
+        {
+            var musteriListe = db.TblMusteri.ToList();
+            return View(musteriListe);
+        }
+    }
+}
