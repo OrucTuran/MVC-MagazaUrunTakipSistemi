1edbaf5edddafff1fcf37e51eb798f4337a5ac80
oructuran0 <orctrn193@gmail.com>
2024-04-27 22:31:51 +0300

    Urun Listeleme Islemleri

---
+++ MagazaUrunTakip/MagazaUrunTakip/Controllers/UrunController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MagazaUrunTakip.Models.Entitiy;
+
+namespace MagazaUrunTakip.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        DbMvcStokEntities db = new DbMvcStokEntities();
+        public ActionResult Index()
+        {
+            var urunler = db.TblUrunler.ToList();
+            return View(urunler);
+        }
+    }
+}
